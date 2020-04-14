2df7f2004cf3d56e70bde32537c88020bbc78859
coskunsahin <coskunsahin1981@yahoo.com>
2020-04-14 21:44:03 +0300

    te

--- OrderCoreReact/OrderCoreReact/Controllers/ValuesController.cs
+++ OrderCoreReact/OrderCoreReact/Controllers/ValuesController.cs
@@ -30,7 +30,18 @@ namespace OrderCoreReact.Controllers
         public void Post([FromBody] string value)
         {
         }
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
 
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
         // PUT api/values/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
